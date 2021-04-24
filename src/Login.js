5a7c298b6e46789a13b3986f7e0eb036456239e8
aloysious x <alicia.todd.army@gmail.com>
2021-04-24 14:40:25 -0700

    preparing for auth0 use

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import LoginButton from './LoginButton.js';
+import LogoutButton from './LogoutButton.js';
 import './Login.css';
 
 class Login extends React.Component {
   render() {
-    return(
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Log In</Card.Title>
-          <Card.Text>
-            Click Below to Log In
+    const { user, isAuthenticated } = this.props.auth0;
+    console.log(user);  
+    return (
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>Log In</Card.Title>
+            <Card.Text>
+              Click Below to Log In
           </Card.Text>
-          {/* TODO: add a `LoginButton` component here that will log the user in with Auth0 */}
-        </Card.Body>
-      </Card>
+            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+            {isAuthenticated ? user.name : ''}
+            {isAuthenticated ? <img src={user.picture} /> : ''}
+            {/* TODO: add a `LoginButton` component here that will log the user in with Auth0 */}
+          </Card.Body>
+        </Card>
+      </div>
     )
   }
 }
 
-export default Login;
+export default withAuth0(Login);
---
+++ src/LoginButton.js
@@ -0,0 +1,18 @@
+'use strict'; 
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <button
+      className="btn btn-primary btn-block"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
+};
+export default LoginButton;
