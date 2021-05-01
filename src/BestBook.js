dee59b7543bdc620a008636e418da952ca3ddc35
aloysious x <alicia.todd.army@gmail.com>
2021-05-01 11:58:33 -0700

    work in progress

--- src/BestBook.js
+++ src/BestBook.js
@@ -7,6 +7,9 @@ import axios from 'axios';
 // C O M P O N E N T S 
 import { withAuth0 } from '@auth0/auth0-react';
 
+// B O O T S T R A P P I N ' 
+import Container from 'react-bootstrap/Container';
+
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
@@ -15,8 +18,8 @@ class BestBooks extends React.Component {
   }
 
   componentDidMount = async () => {
-    const {user} = this.props.auth0;
-    
+    const { user } = this.props.auth0;
+
     console.log('about to request book data');
     const bookData = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/book?user=${user.email}`);
     console.log('book data exists!', bookData);
@@ -25,16 +28,18 @@ class BestBooks extends React.Component {
       books: bookData.favoriteBooks
     });
   }
-}
-render() {
-  return (
-    <>
-      <Container>
-        <h1>📚</h1>
-        {this.state.books && this.state.books.map(book => <h3 key={book._id}>{book.bookName}</h3>)}
-      </Container>
-    </>
-  )
+
+
+  render() {
+    return (
+      <>
+        <Container>
+          <h1>📚</h1>
+          {this.state.book && this.state.book.map(book => <h3 key={book._id}>{book.bookName}</h3>)}
+        </Container>
+      </>
+    )
+  }
 }
 
 export default withAuth0(BestBooks);
